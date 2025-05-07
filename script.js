0f565036a082a982bef11f6257588f9fab0033b7
mstycone <164523886+mstycone@users.noreply.github.com>
2025-05-07 06:44:27 +0200

    MàJ

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ function toggleMenu() {
     menu.classList.toggle('open');
     icon.classList.toggle('open');
 }
+
+document.querySelectorAll('.project-card').forEach(card => {
+    card.addEventListener('click', () => {
+        card.classList.toggle('flipped');
+    });
+});
